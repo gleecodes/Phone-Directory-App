2d9a1a2d6764566cc3e2d9d2d0ec3d671d8d5017
gleecodes <95874981+gleecodes@users.noreply.github.com>
2022-07-12 17:55:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log('Hello!');
+
+
+const addBtn = document.getElementById('submit');
+const contactSummary = document.getElementById('.contactSummary');
+
+
+const name = document.getElementById('name');
+const mobile = document.getElementById('mobile');
+const email = document.getElementById('email');
+
+let ContactArray = [];
+let id = 0;
+
+function Contact(id, name, mobile, email) {
+    this.id = id;
+    this.name = name;
+    this.mobile = mobile;
+    this.email = email;
+}
+
+
+
+addBtn.addEventListener('click', function() {
+    const contact = new Contact(id, name.value, mobile.value, email.value);
+    ContactArray.push(contact);
+    // console.log(contact)
+    localStorage.setItem('contact', JSON.stringify(ContactArray));
+    ClearInputFields();
+
+    
+    console.log(contact);
+var table = document.getElementById("summaryTable");
+var row = table.insertRow(1);
+var cellName = row.insertCell(0);
+var cellMobile = row.insertCell(1);
+var cellEmail = row.insertCell(2);
+
+cellName.innerHTML = `${contact.name}`;
+cellMobile.innerHTML = `${contact.mobile}`;
+cellEmail.innerHTML = `${contact.email}`;
+});
+
+const ClearInputFields = () => {
+    name.value = "",
+    mobile.value = "",
+    email.value = ""
+}
+
+//  get element by phone number
+
+
+
+
+
+
